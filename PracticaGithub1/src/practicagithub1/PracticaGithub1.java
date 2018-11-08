b2fcefa6db0c4f1a814d505f44054baa9e1c0654
admin <admin@Win7_1TB>
2018-11-08 10:56:24 +0100

    Ejercicio7Strings

---
+++ PracticaGithub1/src/practicagithub1/PracticaGithub1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicagithub1;
+import java.util.*;
+/**
+ *
+ * @author admin
+ */
+public class PracticaGithub1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String cadena;
+       int cont=0;
+       
+       
+       
+       Scanner sc=new Scanner (System.in);
+       
+       System.out.println("anota cadena");
+       cadena=sc.nextLine();
+       
+       cadena=cadena.toUpperCase();
+       
+       System.out.println("anota letra");
+       char letra=sc.nextLine().charAt(0);
+       
+       letra=Character.toUpperCase(letra);
+       
+           for(int i=0;i<cadena.length();i++){
+               
+               if(cadena.charAt(i)==letra){
+                   cont++;
+               }
+               
+           }
+       System.out.println("hay "+cont+" letras que son "+letra);
+           
+           
+   }
+    }
+    
+
